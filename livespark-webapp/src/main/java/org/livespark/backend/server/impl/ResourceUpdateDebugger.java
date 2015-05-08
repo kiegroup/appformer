8f5915eee098d0efee3f8be169476a01579209b0
Christian Sadilek <christian.sadilek@gmail.com>
2015-05-08 16:23:27 -0400

    Let's get it started

---
+++ livespark-webapp/src/main/java/org/livespark/backend/server/impl/ResourceUpdateDebugger.java
@@ -0,0 +1,31 @@
+package org.livespark.backend.server.impl;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Named;
+
+import org.uberfire.commons.services.cdi.Startup;
+import org.uberfire.workbench.events.ResourceAddedEvent;
+import org.uberfire.workbench.events.ResourceDeletedEvent;
+import org.uberfire.workbench.events.ResourceRenamedEvent;
+import org.uberfire.workbench.events.ResourceUpdatedEvent;
+
+@Named("debug")
+@Startup
+public class ResourceUpdateDebugger {
+
+    public void onNewFile( @Observes ResourceAddedEvent event ) {
+        System.err.println( "ResourceAddedEvent:" + event.getPath().toURI() + " ['" + event.getMessage() + "']" );
+    }
+
+    public void onUpdateFile( @Observes ResourceUpdatedEvent event ) {
+        System.err.println( "ResourceUpdatedEvent:" + event.getPath().toURI() + " ['" + event.getMessage() + "']" );
+    }
+
+    public void onRenameFile( @Observes ResourceRenamedEvent event ) {
+        System.err.println( "ResourceRenamedEvent:" + event.getPath().toURI() + " -> " + event.getDestinationPath().toURI() + " ['" + event.getMessage() + "']" );
+    }
+
+    public void onDeleteFile( @Observes ResourceDeletedEvent event ) {
+        System.err.println( "ResourceDeletedEvent:" + event.getPath().toURI() + " ['" + event.getMessage() + "']" );
+    }
+}
---
+++ livespark-webapp/src/main/webapp/plugins/home.perspective.js
@@ -0,0 +1,17 @@
+$registerPerspective({
+    id: "Home",
+    is_default: true,
+    panel_type: "org.uberfire.client.workbench.panels.impl.MultiListWorkbenchPanelPresenter",
+    view: {
+        parts: [
+            {
+                place: "welcome",
+                min_height: 100,
+                parameters: {}
+            }
+        ],
+        panels: [
+            
+        ]
+    }
+});
