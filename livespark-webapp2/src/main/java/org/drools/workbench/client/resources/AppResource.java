afe95ef094819e634143159a0d92a8f312052d4e
Walter Medvedeo <wmedvedeo@gmail.com>
2015-05-14 15:35:48 +0200

    Initial version from drools-wb

---
+++ livespark-webapp2/src/main/java/org/drools/workbench/client/resources/AppResource.java
@@ -0,0 +1,12 @@
+package org.drools.workbench.client.resources;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+
+public interface AppResource
+        extends
+        ClientBundle {
+
+    AppResource INSTANCE = GWT.create( AppResource.class );
+
+}
