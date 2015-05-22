67e8515f677a2aaed625c885080bdcd263b3f74e
Max Barkley <mbarkley@redhat.com>
2015-05-22 11:18:55 -0400

    Refactor FormView functionality into base class to be shared with ListItemView

--- livespark-form-modeler/livespark-form-modeler-rendering/livespark-form-modeler-rendering-api/src/main/java/org/livespark/formmodeler/rendering/client/view/ListItemView.java
+++ livespark-form-modeler/livespark-form-modeler-rendering/livespark-form-modeler-rendering-api/src/main/java/org/livespark/formmodeler/rendering/client/view/ListItemView.java
@@ -1,35 +1,7 @@
 package org.livespark.formmodeler.rendering.client.view;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.jboss.errai.databinding.client.api.DataBinder;
-import org.jboss.errai.ui.client.widget.HasModel;
-import org.jboss.errai.ui.shared.api.annotations.AutoBound;
-
-import com.google.gwt.user.client.ui.Composite;
+import org.livespark.formmodeler.rendering.client.shared.FormModel;
 
 
-public abstract class ListItemView<M> extends Composite implements HasModel<M> {
-
-    @Inject
-    @AutoBound
-    protected DataBinder<M> binder;
-
-    protected List<String> inputNames = new ArrayList<String>(  );
-
-    @Override
-    public M getModel() {
-        return binder.getModel();
-    }
-
-    @Override
-    public void setModel( M model ) {
-        binder.setModel( model );
-    }
-
-    protected abstract void initInputNames();
-
+public abstract class ListItemView<M extends FormModel> extends BaseView<M> {
 }
