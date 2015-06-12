daac504bc20678b9fd8647f840d5c7875d45f412
Max Barkley <mbarkley@redhat.com>
2015-06-12 16:48:22 -0400

    Generate implementation for getDataModels in FormModel.

--- livespark-form-modeler/livespark-form-modeler-codegen/livespark-form-modeler-codegen-impl/src/main/java/org/livespark/formmodeler/codegen/model/impl/RoasterFormModelSourceGenerator.java
+++ livespark-form-modeler/livespark-form-modeler-codegen/livespark-form-modeler-codegen-impl/src/main/java/org/livespark/formmodeler/codegen/model/impl/RoasterFormModelSourceGenerator.java
@@ -7,6 +7,10 @@ import static org.livespark.formmodeler.codegen.util.SourceGenerationUtil.INJECT
 import static org.livespark.formmodeler.codegen.util.SourceGenerationUtil.VALIDATION_NOT_NULL;
 import static org.livespark.formmodeler.codegen.util.SourceGenerationUtil.VALIDATION_VALID;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -14,6 +18,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.jboss.forge.roaster.Roaster;
 import org.jboss.forge.roaster.model.source.FieldSource;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.MethodSource;
 import org.livespark.formmodeler.codegen.SourceGenerationContext;
 import org.livespark.formmodeler.codegen.model.FormModelSourceGenerator;
 import org.livespark.formmodeler.model.DataHolder;
@@ -32,21 +37,76 @@ public class RoasterFormModelSourceGenerator implements FormModelSourceGenerator
     public String generateFormModelSource( SourceGenerationContext context ) {
 
         JavaClassSource modelClass = Roaster.create( JavaClassSource.class );
+
+        addTypeSignature( context, modelClass );
+        addImports( context, modelClass );
+        addTypeAnnotations( context, modelClass );
+        addProperties( context, modelClass );
+        addConstructors( context, modelClass );
+        addMethodImpls( context, modelClass );
+
+        return modelClass.toString();
+    }
+
+    private void addImports( SourceGenerationContext context,
+                             JavaClassSource modelClass ) {
+        modelClass.addImport( List.class );
+        modelClass.addImport( Arrays.class );
+    }
+
+    private void addMethodImpls( SourceGenerationContext context,
+                                 JavaClassSource modelClass ) {
+        addGetDataModelsImpl( context, modelClass );
+    }
+
+    private void addGetDataModelsImpl( SourceGenerationContext context,
+                                       JavaClassSource modelClass ) {
+        MethodSource<JavaClassSource> getDataModels = modelClass.addMethod();
+        getDataModels.setPublic()
+                     .setName( "getDataModels" )
+                     .setReturnType( "List<Object>" )
+                     .setBody( generateGetDataModelsBody( context ) )
+                     .addAnnotation( Override.class );
+    }
+
+    private String generateGetDataModelsBody( SourceGenerationContext context ) {
+        StringBuilder body = new StringBuilder();
+
+        body.append( "return Arrays.<Object>asList(" );
+
+        Iterator<DataHolder> iter = context.getFormDefinition().getDataHolders().iterator();
+        while ( iter.hasNext() ) {
+            DataHolder holder = iter.next();
+            body.append( holder.getName() );
+            if ( iter.hasNext() ) {
+                body.append( ", " );
+            }
+        }
+        body.append( ");" );
+
+        return body.toString();
+    }
+
+    private void addConstructors( SourceGenerationContext context,
+                            JavaClassSource modelClass ) {
+        constructorGenerator.addNoArgConstructor( modelClass );
+        constructorGenerator.addFormModelConstructor( context, modelClass );
+    }
+
+    private void addTypeAnnotations( SourceGenerationContext context,
+                            JavaClassSource modelClass ) {
+        modelClass.addAnnotation( ERRAI_PORTABLE );
+        modelClass.addAnnotation( ERRAI_BINDABLE );
+        modelClass.addAnnotation( INJECT_NAMED ).setStringValue( context.getModelName() );
+    }
+
+    private void addTypeSignature( SourceGenerationContext context,
+                            JavaClassSource modelClass ) {
         modelClass.setPackage( context.getSharedPackage().getPackageName() )
                 .setPublic()
                 .setName( context.getModelName() );
 
         modelClass.setSuperType( FORM_MODEL_CLASS );
-
-        modelClass.addAnnotation( ERRAI_PORTABLE );
-        modelClass.addAnnotation( ERRAI_BINDABLE );
-        modelClass.addAnnotation( INJECT_NAMED ).setStringValue( context.getModelName() );
-
-        addProperties( context, modelClass );
-        constructorGenerator.addNoArgConstructor( modelClass );
-        constructorGenerator.addFormModelConstructor( context, modelClass );
-
-        return modelClass.toString();
     }
 
     private void addProperties( SourceGenerationContext context, JavaClassSource modelClass ) {
