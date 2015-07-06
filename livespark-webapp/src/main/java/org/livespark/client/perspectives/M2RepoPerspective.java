f3e365e13bc321b7c0cf925a7b59c7e0961e6807
Max Barkley <mbarkley@redhat.com>
2015-07-06 12:16:05 -0400

    Fix compile error from change in guvnor snapshots.

--- livespark-webapp/src/main/java/org/livespark/client/perspectives/M2RepoPerspective.java
+++ livespark-webapp/src/main/java/org/livespark/client/perspectives/M2RepoPerspective.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 JBoss Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,17 +20,13 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
-import com.google.gwt.event.logical.shared.CloseEvent;
-import com.google.gwt.event.logical.shared.CloseHandler;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.PopupPanel;
-import org.livespark.client.resources.i18n.AppConstants;
 import org.guvnor.m2repo.client.event.M2RepoRefreshEvent;
 import org.guvnor.m2repo.client.event.M2RepoSearchEvent;
-import org.guvnor.m2repo.client.upload.UploadForm;
+import org.guvnor.m2repo.client.upload.UploadFormView;
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.kie.workbench.common.widgets.client.search.ContextualSearch;
 import org.kie.workbench.common.widgets.client.search.SearchBehavior;
+import org.livespark.client.resources.i18n.AppConstants;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPanel;
 import org.uberfire.client.annotations.WorkbenchPerspective;
@@ -41,6 +37,11 @@ import org.uberfire.mvp.Command;
 import org.uberfire.security.annotations.Roles;
 import org.uberfire.workbench.model.menu.MenuFactory;
 import org.uberfire.workbench.model.menu.Menus;
+
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.PopupPanel;
 
 /**
  * A Perspective to show M2_REPO related screen
@@ -81,16 +82,16 @@ public class M2RepoPerspective extends FlowPanel {
                 .respondsWith( new Command() {
                     @Override
                     public void execute() {
-                        final UploadForm uploadForm = iocManager.lookupBean( UploadForm.class ).getInstance();
+                        final UploadFormView uploadForm = iocManager.lookupBean( UploadFormView.class ).getInstance();
                         //When pop-up is closed destroy bean to avoid memory leak
-                        uploadForm.addCloseHandler( new CloseHandler<PopupPanel>() {
+                        uploadForm.asWidget().addHandler( new CloseHandler<PopupPanel>() {
 
                             @Override
                             public void onClose( CloseEvent<PopupPanel> event ) {
                                 iocManager.destroyBean( uploadForm );
                             }
 
-                        } );
+                        }, CloseEvent.getType() );
                         uploadForm.show();
                     }
                 } )
