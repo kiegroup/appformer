cc3389883d8de0ff92e1a472303c66209de57463
Max Barkley <mbarkley@redhat.com>
2015-07-09 14:11:44 -0400

    Lookup livespark-webapp via maven resolver in integration tests.

--- livespark-integration-tests/src/test/java/org/livespark/test/BaseIntegrationTest.java
+++ livespark-integration-tests/src/test/java/org/livespark/test/BaseIntegrationTest.java
@@ -1,6 +1,7 @@
 package org.livespark.test;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -15,17 +16,36 @@ import org.jboss.errai.bus.server.api.RpcContext;
 import org.jboss.errai.security.shared.service.AuthenticationService;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.kie.workbench.common.services.shared.project.KieProjectService;
 import org.livespark.test.mock.MockQueueSession;
 import org.uberfire.io.IOService;
+
+import com.google.common.io.Files;
 
 public class BaseIntegrationTest {
 
     public static WebArchive createLiveSparkDeployment() {
         /*
-         * FIXME This file only exists if livespark-webapp has been built prior to this test.
+         * FIXME Don't use hard-coded version.
          */
-        return ShrinkWrap.createFromZipFile( WebArchive.class, new File( "../livespark-webapp/target/livespark-webapp.war" ) )
+        final File warFile = Maven.resolver()
+                                  .resolve( "org.livespark:livespark-webapp:war:0.0.1-SNAPSHOT" )
+                                  .withoutTransitivity()
+                                  .asSingleFile();
+
+        final File targetWarFile = new File( "target/livespark-webapp.war" );
+
+        /*
+         * If we do not copy the war file into the target directory this error causes tests to fail: JBAS016071
+         */
+        try {
+            Files.copy( warFile, targetWarFile );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+
+        return ShrinkWrap.createFromZipFile( WebArchive.class, targetWarFile )
                          .addClasses( BaseIntegrationTest.class, MockQueueSession.class );
     }
 
