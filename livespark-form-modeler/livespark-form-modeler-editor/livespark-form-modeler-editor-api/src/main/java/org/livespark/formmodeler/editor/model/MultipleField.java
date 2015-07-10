5f35f0447ccdb355f85f3e208eb8f9273f431f23
Max Barkley <mbarkley@redhat.com>
2015-07-10 14:22:45 -0400

    Add missing license headers.

--- livespark-form-modeler/livespark-form-modeler-editor/livespark-form-modeler-editor-api/src/main/java/org/livespark/formmodeler/editor/model/MultipleField.java
+++ livespark-form-modeler/livespark-form-modeler-editor/livespark-form-modeler-editor-api/src/main/java/org/livespark/formmodeler/editor/model/MultipleField.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.livespark.formmodeler.editor.model;
-
 /**
  * Created by pefernan on 7/2/15.
  */
