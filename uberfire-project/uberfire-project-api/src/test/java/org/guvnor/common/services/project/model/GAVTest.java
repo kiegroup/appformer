apache/incubator-kie-tools

commit 377b3dec5d2337d3b4ff77815084e1447482c7d2

AF-1230: Prevent couple of exceptions in uberfire-project-api (#388)
Just looked at ubefrife-project-api and found couple of lurking NPEs and ArrayIndexOutOfBoundsExceptions. Reproducers and fixes included + expanded a coverage a bit.

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/GAV.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Red Hat, Inc. and/or its affiliates.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,8 +17,11 @@
 package org.guvnor.common.services.project.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
+
+import static org.kie.soup.commons.validation.PortablePreconditions.checkNotNull;
 
 @Portable
 public class GAV implements Serializable {
@@ -34,7 +37,11 @@ public class GAV implements Serializable {
     }
 
     public GAV(final String gavString) {
-        String[] split = gavString.split(":");
+        String[] split = checkNotNull("gavString", gavString)
+                .split(":");
+        if (split.length != 3) {
+            throw new IllegalArgumentException("The GAV String must contain groupId artifactId and version separated by ':', but it was " + gavString);
+        }
         this.groupId = split[0];
         this.artifactId = split[1];
         this.version = split[2];
@@ -82,7 +89,7 @@ public class GAV implements Serializable {
     }
 
     public boolean isSnapshot() {
-        return this.version.endsWith("-SNAPSHOT");
+        return version != null && version.endsWith("-SNAPSHOT");
     }
 
     @Override

uberfire-project/uberfire-project-api/src/test/java/org/guvnor/common/services/project/model/GAVTest.java
@@ -17,19 +17,47 @@ package org.guvnor.common.services.project.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GAVTest {
 
     @Test
-    public void testGAVFromString() throws Exception {
+    public void testGAVFromString() {
         GAV gav = new GAV("myGroupID:myArtifactID:version");
 
-        assertEquals("myGroupID",
-                     gav.getGroupId());
-        assertEquals("myArtifactID",
-                     gav.getArtifactId());
-        assertEquals("version",
-                     gav.getVersion());
+        assertThat(gav.getGroupId()).isEqualTo("myGroupID");
+        assertThat(gav.getArtifactId()).isEqualTo("myArtifactID");
+        assertThat(gav.getVersion()).isEqualTo("version");
+    }
+
+    @Test
+    public void whenGivenNullString_throwsIllegalArgumentException() {
+        assertThatThrownBy(() -> new GAV(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Parameter named 'gavString' should be not null!");
+    }
+
+    @Test
+    public void whenGivenInvalidGav_throwsIllegalArgumentExceptions() {
+        assertThatThrownBy(() -> new GAV("nonsense"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("The GAV String must contain groupId artifactId and version separated by ':', but it was nonsense");
+    }
+
+    @Test
+    public void isSnapshotTest() {
+        GAV
+                noSnapshot1 = new GAV(),
+                noSnapshot2 = new GAV("group:artifact:1.0"),
+                noSnapshot3 = new GAV("group", "artifact", "version"),
+                noSnapshot4 = new GAV("group", "artifact", null),
+                snapshot = new GAV("group", "artifact", "1.0-SNAPSHOT");
+
+        assertThat(noSnapshot1.isSnapshot()).isFalse();
+        assertThat(noSnapshot2.isSnapshot()).isFalse();
+        assertThat(noSnapshot3.isSnapshot()).isFalse();
+        assertThat(noSnapshot4.isSnapshot()).isFalse();
+        assertThat(snapshot.isSnapshot()).isTrue();
     }
 }

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/Build.java
@@ -16,19 +16,20 @@
 package org.guvnor.common.services.project.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
 
 @Portable
 public class Build {
 
-    private ArrayList<Plugin> plugins = new ArrayList<Plugin>();
+    private List<Plugin> plugins = new ArrayList<>();
 
-    public ArrayList<Plugin> getPlugins() {
+    public List<Plugin> getPlugins() {
         return plugins;
     }
 
-    public void setPlugins(ArrayList<Plugin> plugins) {
+    public void setPlugins(List<Plugin> plugins) {
         this.plugins = plugins;
     }
 }

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/Dependencies.java
@@ -24,6 +24,8 @@ import java.util.ListIterator;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
 
+import static java.util.stream.Collectors.toList;
+
 @Portable
 public class Dependencies
         implements List<Dependency> {
@@ -31,7 +33,7 @@ public class Dependencies
     private final List<Dependency> dependencies;
 
     public Dependencies() {
-        dependencies = new ArrayList<Dependency>();
+        dependencies = new ArrayList<>();
     }
 
     public Dependencies(final List<Dependency> dependencies) {
@@ -58,26 +60,22 @@ public class Dependencies
     }
 
     public Collection<GAV> getGavs(final String... scopes) {
-        final List<String> scopesAsList = Arrays.asList(scopes);
+        final List<String> scopesList = Arrays.asList(scopes);
 
-        if (scopesAsList.isEmpty()) {
-            return new ArrayList<GAV>(dependencies);
+        if (scopesList.isEmpty()) {
+            return new ArrayList<>(dependencies);
         } else {
-            final ArrayList<GAV> result = new ArrayList<GAV>();
-
-            for (Dependency dependency : dependencies) {
-                if (scopesAsList.contains(dependency.getScope())) {
-                    result.add(dependency);
-                }
-            }
-
-            return result;
+            return dependencies.stream()
+                    .filter(dep -> scopesList.contains(dep.getScope()))
+                    .collect(toList());
         }
     }
 
     public Collection<GAV> getCompileScopedGavs() {
         return getGavs("compile",
-                       null);
+                       // When scope is not declared (dependency.getScope() == null), maven considers it to be compile scope
+                       null
+        );
     }
 
     @Override
@@ -160,15 +158,12 @@ public class Dependencies
     @Override
     public Dependency set(int i,
                           Dependency dependency) {
-        return dependencies.set(i,
-                                dependency);
+        return dependencies.set(i, dependency);
     }
 
     @Override
-    public void add(int i,
-                    Dependency dependency) {
-        dependencies.add(i,
-                         dependency);
+    public void add(int i, Dependency dependency) {
+        dependencies.add(i, dependency);
     }
 
     @Override
@@ -199,7 +194,6 @@ public class Dependencies
     @Override
     public List<Dependency> subList(int i,
                                     int i1) {
-        return dependencies.subList(i,
-                                    i1);
+        return dependencies.subList(i, i1);
     }
 }

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/Module.java
@@ -34,7 +34,7 @@ public class Module {
 
     protected Path rootPath;
     protected Path pomXMLPath;
-    protected Collection<String> modules = new ArrayList<String>();
+    protected Collection<String> modules = new ArrayList<>();
     protected POM pom;
 
     // only loaded when listing projects
@@ -49,8 +49,7 @@ public class Module {
                   final POM pom) {
         this(rootPath,
              pomXMLPath);
-        this.pom = checkNotNull("pom",
-                                pom);
+        this.pom = checkNotNull("pom", pom);
     }
 
     public Module(final Path rootPath,
@@ -65,10 +64,8 @@ public class Module {
 
     public Module(Path rootPath,
                   Path pomXMLPath) {
-        this.rootPath = checkNotNull("rootPath",
-                                     rootPath);
-        this.pomXMLPath = checkNotNull("pomXMLPath",
-                                       pomXMLPath);
+        this.rootPath = checkNotNull("rootPath", rootPath);
+        this.pomXMLPath = checkNotNull("pomXMLPath", pomXMLPath);
     }
 
     public int getNumberOfAssets() {
@@ -114,7 +111,7 @@ public class Module {
     }
 
     public void setPom(POM pom) {
-        this.pom = pom;
+        this.pom = checkNotNull("pom", pom);
     }
 
     @Override

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/ModuleRepositories.java
@@ -28,7 +28,7 @@ public class ModuleRepositories {
     private Set<ModuleRepository> repositories;
 
     public ModuleRepositories() {
-        this.repositories = new HashSet<ModuleRepository>();
+        this.repositories = new HashSet<>();
     }
 
     public ModuleRepositories(final @MapsTo("repositories") Set<ModuleRepository> repositories) {
@@ -40,7 +40,7 @@ public class ModuleRepositories {
     }
 
     public MavenRepositoryMetadata[] filterByIncluded() {
-        final Set<MavenRepositoryMetadata> filter = new HashSet<MavenRepositoryMetadata>();
+        final Set<MavenRepositoryMetadata> filter = new HashSet<>();
         for (ModuleRepository pr : repositories) {
             if (pr.isIncluded()) {
                 filter.add(pr.getMetadata());

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/POM.java
@@ -36,9 +36,9 @@ public class POM {
 
     private Build build;
 
-    private List<Dependency> dependencies = new ArrayList<Dependency>();
-    private List<MavenRepository> repositories = new ArrayList<MavenRepository>();
-    private List<String> modules = new ArrayList<String>();
+    private List<Dependency> dependencies = new ArrayList<>();
+    private List<MavenRepository> repositories = new ArrayList<>();
+    private List<String> modules = new ArrayList<>();
 
     public POM() {
         this.gav = new GAV();
@@ -70,7 +70,6 @@ public class POM {
                final String url,
                final GAV gav,
                final boolean multiModule) {
-        super();
         this.name = name;
         this.description = description;
         this.url = url;
@@ -153,7 +152,7 @@ public class POM {
     }
 
     public boolean isMultiModule() {
-        return packaging.equals("pom");
+        return "pom".equals(packaging);
     }
 
     public void setPackaging(String packaging) {

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/Plugin.java
@@ -23,7 +23,7 @@ import org.jboss.errai.common.client.api.annotations.Portable;
 @Portable
 public class Plugin {
 
-    private List<Dependency> dependencies = new ArrayList<Dependency>();
+    private List<Dependency> dependencies = new ArrayList<>();
     private String groupId;
     private String artifactId;
     private String version;

kiegroup/appformer

commit a9c00fc9e31c94b4e934dfb116226a228f065e46

Fix regression introduced by (#388)

uberfire-project/uberfire-project-api/src/main/java/org/guvnor/common/services/project/model/GAV.java
@@ -39,8 +39,8 @@ public class GAV implements Serializable {
     public GAV(final String gavString) {
         String[] split = checkNotNull("gavString", gavString)
                 .split(":");
-        if (split.length != 3) {
-            throw new IllegalArgumentException("The GAV String must contain groupId artifactId and version separated by ':', but it was " + gavString);
+        if (split.length < 3) {
+            throw new IllegalArgumentException("The GAV String must have the form group:artifact:version[:scope] but was '" + gavString + "'");
         }
         this.groupId = split[0];
         this.artifactId = split[1];

uberfire-project/uberfire-project-api/src/test/java/org/guvnor/common/services/project/model/GAVTest.java
@@ -39,12 +39,29 @@ public class GAVTest {
     }
 
     @Test
+    public void createGavFromString() {
+        GAV gav1 = new GAV("gr1:ar1:ver1");
+        assertThat(gav1.getGroupId()).isEqualTo("gr1");
+        assertThat(gav1.getArtifactId()).isEqualTo("ar1");
+        assertThat(gav1.getVersion()).isEqualTo("ver1");
+
+        GAV gav2 = new GAV("gr2:ar2:ver2:compile");
+        assertThat(gav2.getGroupId()).isEqualTo("gr2");
+        assertThat(gav2.getArtifactId()).isEqualTo("ar2");
+        assertThat(gav2.getVersion()).isEqualTo("ver2");
+    }
+
+    @Test
     public void whenGivenInvalidGav_throwsIllegalArgumentExceptions() {
         assertThatThrownBy(() -> new GAV("nonsense"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("The GAV String must contain groupId artifactId and version separated by ':', but it was nonsense");
-    }
+                .hasMessage("The GAV String must have the form group:artifact:version[:scope] but was 'nonsense'");
 
+        assertThatThrownBy(() -> new GAV("mygroup:myartifact"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("The GAV String must have the form group:artifact:version[:scope] but was 'mygroup:myartifact'");
+    }
+    
     @Test
     public void isSnapshotTest() {
         GAV