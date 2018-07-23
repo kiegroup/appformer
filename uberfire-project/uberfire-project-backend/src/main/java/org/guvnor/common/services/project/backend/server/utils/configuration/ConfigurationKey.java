72dc81d513037a9f712ed575f778c04ef2285596
Massimiliano <desmax74@yahoo.it>
2018-07-23 10:37:02 +0200

    javadoc

--- uberfire-project/uberfire-project-backend/src/main/java/org/guvnor/common/services/project/backend/server/utils/configuration/ConfigurationKey.java
+++ uberfire-project/uberfire-project-backend/src/main/java/org/guvnor/common/services/project/backend/server/utils/configuration/ConfigurationKey.java
@@ -16,6 +16,29 @@
 
 package org.guvnor.common.services.project.backend.server.utils.configuration;
 
+/**
+ * Keys use to configure the Maven compiler in the kie-wb-common
+ *
+ * <p>
+ *  COMPILER jdt or javac
+ *  SOURCE_VERSION  configured with jvm source code version
+ *  TARGET_VERSION  configured with jvm target version
+ *  <p>
+ *  FAIL_ON_ERROR configured with false to continue the build on the correct classes and skip the build of classes with errors
+ *  MAVEN_COMPILER_PLUGIN_GROUP  configured with default maven compiler group to disabled it
+ *  MAVEN_COMPILER_PLUGIN_ARTIFACT configured with default maven compiler ArtifactID to disabled it
+ *  MAVEN_COMPILER_PLUGIN_VERSION configured with default maven compiler version to disabled it
+ *  <p>
+ *  TAKARI_COMPILER_PLUGIN_GROUP configured with takari GroupID
+ *  TAKARI_COMPILER_PLUGIN_ARTIFACT configured with takari ArtifactID
+ *  TAKARI_COMPILER_PLUGIN_VERSION configured with a placeholder and set with the correct value in the maven build with takari version
+ *  <p>
+ *  KIE_MAVEN_PLUGINS configured with the GroupID of kie plugin from the Integration prj
+ *  KIE_MAVEN_PLUGIN  configured with the artifactID of the kie-maven-plugin
+ *  KIE_TAKARI_PLUGIN configured with the artifactID of the kie-takari-plugin
+ *  <p>
+ *  KIE_VERSION configured with a placeholder and set with the correct value in the maven build
+ * */
 public enum ConfigurationKey {
 
     COMPILER,
