65fdc593d76dc73dad1f3920c3984ea29e036e9f
Massimiliano <desmax74@yahoo.it>
2018-09-06 12:37:03 +0200

    Added test

--- uberfire-m2repo-editor/uberfire-m2repo-editor-backend/src/main/java/org/guvnor/m2repo/backend/server/M2ServletContextListener.java
+++ uberfire-m2repo-editor/uberfire-m2repo-editor-backend/src/main/java/org/guvnor/m2repo/backend/server/M2ServletContextListener.java
@@ -15,17 +15,29 @@
  */
 package org.guvnor.m2repo.backend.server;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import javax.inject.Inject;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.appformer.maven.integration.Aether;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.installation.InstallRequest;
+import org.eclipse.aether.installation.InstallResult;
+import org.eclipse.aether.installation.InstallationException;
 import org.guvnor.common.services.project.model.GAV;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,42 +52,100 @@ public class M2ServletContextListener implements ServletContextListener {
     private final String JAR_EXT = ".jar";
     private final String WEB_INF_FOLDER = "WEB-INF";
     private final String LIB_FOLDER = "lib";
-    private final String CTX_JARS = "CTX_JARS";
+    private final String JAR_ARTIFACT = "jar";
+    private final String GROUP_ID = "groupId";
+    private final String ARTIFACT_ID = "artifactId";
+    private final String VERSION = "version";
+
     private Logger logger = LoggerFactory.getLogger(M2ServletContextListener.class);
 
-    @Inject
-    private GuvnorM2Repository repository;
-
+    public M2ServletContextListener(){}
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        logger.info("M2ServletContextListener contextInitialized started");
         ServletContext ctx = servletContextEvent.getServletContext();
         String jarsPath = ctx.getRealPath(getJarsFolder());
-        deployJarFromWar(jarsPath, ctx);
-        logger.info("M2ServletContextListener contextInitialized");
+        long startTime = System.nanoTime();
+        int jarsDeployed = deployJarsFromWar(jarsPath);
+        long endTime = System.nanoTime();
+        long totalTime = TimeUnit.NANOSECONDS.toSeconds(endTime - startTime);
+        logger.info("M2ServletContextListener contextInitialized deployed {} jars in {} sec ", jarsDeployed, totalTime);
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        servletContextEvent.getServletContext().removeAttribute(CTX_JARS);
-    }
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {}
 
     private String getJarsFolder(){
         StringBuilder sb = new StringBuilder();
-        sb.append(File.separator).append(WEB_INF_FOLDER).append(File.separator).append(LIB_FOLDER);
+        sb.append(File.separator).append(WEB_INF_FOLDER).append(File.separator).append(LIB_FOLDER).append(File.separator);
         return sb.toString();
     }
 
-    public void deployJarFromWar(String path, ServletContext ctx) {
-        try (DirectoryStream<Path> ds = Files.newDirectoryStream(Paths.get("file://" + path))) {
+    private int deployJarsFromWar(String path) {
+        int i = 0;
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(Paths.get(path))) {
             for (Path p : ds) {
-                if (!Files.isDirectory(p) && p.endsWith(JAR_EXT)) {
-                    GAV gav = repository.loadGAVFromJar(p.toAbsolutePath().toString());
-                    repository.deployPom(ctx.getResourceAsStream(p.toAbsolutePath().toString()), gav);
+                if (p.toString().endsWith(JAR_EXT)) {
+                    deployJar(p.toAbsolutePath().toString());
+                    i++;
                 }
             }
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
+        return i;
+    }
+
+    public GAV deployJar(String file) {
+        GAV gav = new GAV();
+        Properties props = readZipFile(file);
+        if(!props.isEmpty()){
+            gav = new GAV(props.getProperty(GROUP_ID),props.getProperty(ARTIFACT_ID), props.getProperty(VERSION) );
+            deploy(gav, file);
+        }
+        return gav;
+    }
+
+
+    public Properties readZipFile(String zipFilePath) {
+        try {
+            ZipFile zipFile = new ZipFile(zipFilePath);
+            Enumeration<? extends ZipEntry> e = zipFile.entries();
+            while (e.hasMoreElements()) {
+                ZipEntry entry = e.nextElement();
+                String begin = "META-INF"+ File.separator + "maven";
+                String end = "pom.properties";
+                if (!entry.isDirectory() && entry.getName().startsWith(begin) && entry.getName().endsWith(end)) {
+                    BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));
+                    Properties props = new Properties();
+                    props.load(bis);
+                    bis.close();
+                    return props;
+                } else {
+                    continue;
+                }
+            }
+        } catch (IOException e) {
+            logger.error("IOError :{}",e.getMessage(), e);
+        }
+        return new Properties();
+    }
+
+    public boolean  deploy(GAV gav, String jarFile) {
+        Artifact jarArtifact = new DefaultArtifact(gav.getGroupId(),
+                                                   gav.getArtifactId(),
+                                                   JAR_ARTIFACT,
+                                                   gav.getVersion());
+        jarArtifact = jarArtifact.setFile(new File(jarFile));
+        try {
+            final InstallRequest installRequest = new InstallRequest();
+            installRequest.addArtifact(jarArtifact);
+            InstallResult result = Aether.getAether().getSystem().install(Aether.getAether().getSession(), installRequest);
+            return result.getArtifacts().size() == 1;
+        } catch (InstallationException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return false;
     }
 }
--- uberfire-m2repo-editor/uberfire-m2repo-editor-backend/src/test/java/org/guvnor/m2repo/backend/server/M2ServletContextListenerTest.java
+++ uberfire-m2repo-editor/uberfire-m2repo-editor-backend/src/test/java/org/guvnor/m2repo/backend/server/M2ServletContextListenerTest.java
@@ -16,106 +16,77 @@
 package org.guvnor.m2repo.backend.server;
 
 import java.io.File;
-import java.net.URL;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import org.appformer.maven.integration.Aether;
 
-import org.guvnor.m2repo.backend.server.helpers.HttpGetHelper;
-import org.guvnor.m2repo.backend.server.helpers.HttpPostHelper;
-import org.guvnor.m2repo.backend.server.helpers.HttpPutHelper;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.guvnor.common.services.project.model.GAV;
+import org.junit.After;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-@RunWith(Arquillian.class)
 public class M2ServletContextListenerTest {
 
-    private static Logger logger = LoggerFactory.getLogger(M2ServletContextListenerTest.class);
-
-    @ArquillianResource
-    private URL deploymentUrl;
-
-    @Deployment
-    public static Archive getDeployment() {
-
-        final WebArchive war = ShrinkWrap.create(WebArchive.class, "M2.war");
-        final File[] metaInfFilesFiles;
-
-        war.setWebXML(new File("target/test-classes/web.xml"));
-        metaInfFilesFiles = new File("target/test-classes/META-INF").listFiles();
-
-
-        war.addClasses(M2ServletContextListener.class, M2Servlet.class,
-                       HttpPostHelper.class, HttpPutHelper.class, HttpGetHelper.class, ApplicationScopedProducer.class);
-
-
-       // war.addPackages(true, "org.guvnor.m2repo.backend.server");
-        for (final File file : metaInfFilesFiles) {
-            war.addAsManifestResource(file);
-        }
-
-        String settings = "src/test/settings.xml" ;
-
-        final File[] files = Maven.configureResolver().
-                fromFile(settings).
-                loadPomFromFile("./pom.xml")
-                .resolve(
-                        "org.assertj:assertj-core:?",
-                        "org.apache.maven:maven-core:?",
-                        "org.apache.maven:maven-model:?",
-                        "org.apache.maven:maven-settings:?",
-
-                        "com.google.inject.extensions:guice-servlet:?",
-                        "org.codehaus.plexus:plexus-utils:?",
-
-                        "org.eclipse.aether:aether-api:?",
-                        "org.eclipse.aether:aether-util:?",
-
-                        "org.uberfire:uberfire-m2repo-editor-backend:?",
-                        "org.uberfire:uberfire-project-backend:?",
-                        "org.uberfire:uberfire-server:?",
-                        "org.uberfire:uberfire-project-api:?",
-                        "org.uberfire:uberfire-servlet-security:?",
-                        "org.uberfire:uberfire-commons-editor-backend:?",
-                        "org.uberfire:uberfire-structure-backend:?",
-                        "org.uberfire:uberfire-project-backend:?",
-
-                        "org.jboss.errai:errai-jboss-as-support:?",
-                        "org.jboss.errai:errai-security-server:?",
-
-                        "commons-io:commons-io:?",
-                        "com.thoughtworks.xstream:xstream:?").withTransitivity()
-                .asFile();
-
-        for (final File file : files) {
-            war.addAsLibrary(file);
-        }
-        System.out.println(war.toString(true));
-        return war;
+    @After
+    public void tearDown() throws Exception{
+        deleteArtifactIFPresent();
     }
 
-    @Test
-    public void getTest() {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target(deploymentUrl.toString() + "/maven2/");
-        Invocation invocation = target.request().buildGet();
-        Response response = invocation.invoke();
-        assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(200);
-        //assertThat(response.readEntity(String.class)).isEqualTo(maven);
+    private void deleteArtifactIFPresent() throws ArtifactResolutionException {
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setArtifact(getArtifact());
+        ArtifactResult result = Aether.getAether().getSystem().resolveArtifact(Aether.getAether().getSession(), artifactRequest);
+        if(!result.isMissing()){
+            File artifactFile = result.getArtifact().getFile();
+            assertThat(artifactFile.delete());
+        }
+    }
 
+    private Artifact getArtifact(){
+        Artifact jarArtifact = new DefaultArtifact("org.uberfire",
+                                                   "uberfire-m2repo-editor-backend",
+                                                   "jar",
+                                                   "100-SNAPSHOT");
+        return jarArtifact;
+    }
+
+
+    @Test
+    public void deployJarsFrowWar() throws Exception {
+        assertThat(checksIfArtifactIsPresent()).isFalse();
+
+
+        File file = new File("target/test-classes/org/guvnor/m2repo/backend/server/uberfire-m2repo-editor-backend-100-SNAPSHOT.jar");
+        assertThat(file).exists();
+
+        M2ServletContextListener listener = new M2ServletContextListener();
+        GAV deployed = listener.deployJar(file.getAbsolutePath());
+        assertThat(deployed.getGroupId()).isEqualTo("org.uberfire");
+        assertThat(deployed.getArtifactId()).isEqualTo("uberfire-m2repo-editor-backend");
+        assertThat(deployed.getVersion()).isEqualTo("100-SNAPSHOT");
+
+
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setArtifact(getArtifact());
+        ArtifactResult result = Aether.getAether().getSystem().resolveArtifact(Aether.getAether().getSession(), artifactRequest);
+        assertThat(result.isMissing()).isFalse();
+        assertThat(result.isResolved()).isTrue();
+    }
+
+    private boolean checksIfArtifactIsPresent() {
+        try {
+            ArtifactRequest artifactRequest = new ArtifactRequest();
+            artifactRequest.setArtifact(getArtifact());
+            Aether.getAether().getSystem().resolveArtifact(Aether.getAether().getSession(),
+                                                                                   artifactRequest);
+            return true;
+        }catch (ArtifactResolutionException e){
+            return false;
+        }
     }
 }
