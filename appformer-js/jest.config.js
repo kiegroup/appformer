1a2b53e727b2a19906446ab1439716bb8cad0a6a
Tiago Bento <tiagobentofernandes@gmail.com>
2019-03-12 13:35:18 -0300

    Jest-junit

--- appformer-js/jest.config.js
+++ appformer-js/jest.config.js
@@ -15,11 +15,12 @@
  */
 
 module.exports = {
-    moduleDirectories: ["node_modules", "src"],
-    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-    testRegex: "/__tests__/.*\\.test\\.(jsx?|tsx?)$",
-    transform: {
-        "^.+\\.jsx?$": "babel-jest",
-        "^.+\\.tsx?$": "ts-jest"
-    }
+  reporters: ["default", "jest-junit"],
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  testRegex: "/__tests__/.*\\.test\\.(jsx?|tsx?)$",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest"
+  }
 };
