e8604f7ee45c50e1920cb1e83cffe036b3229e01
Abhishek Kumar <akumar074@gmail.com>
2019-12-03 18:21:15 +0530

    Column Id added to new column while replacing ColumnWithComponents to ComponentColumn

--- uberfire-extensions/uberfire-layout-editor/uberfire-layout-editor-client/src/main/java/org/uberfire/ext/layout/editor/client/components/rows/Row.java
+++ uberfire-extensions/uberfire-layout-editor/uberfire-layout-editor-client/src/main/java/org/uberfire/ext/layout/editor/client/components/rows/Row.java
@@ -405,11 +405,11 @@ public class Row implements LayoutEditorElement {
         Optional<Column> optional = checkIfColumnExistsInChildColumnWithComponents(targetColumn);
 
         // If present let's remove it!
-        optional.ifPresent(column -> removeComponentFromColumnWihtComponents((ColumnWithComponents) column,
+        optional.ifPresent(column -> removeComponentFromColumnWithComponents((ColumnWithComponents) column,
                                                                              targetColumn));
     }
 
-    private void removeComponentFromColumnWihtComponents(ColumnWithComponents parent,
+    private void removeComponentFromColumnWithComponents(ColumnWithComponents parent,
                                                          Column targetColumn) {
         PortablePreconditions.checkNotNull("parent",
                                            parent);
@@ -436,21 +436,17 @@ public class Row implements LayoutEditorElement {
         if (columnToReplace.getRow().getColumns().size() == 1) {
 
             // getting the remaining column
-            Column column = columnToReplace.getRow().getColumns().remove(0);
+            ComponentColumn originalColumn = (ComponentColumn) columnToReplace.getRow().getColumns().remove(0);
+            ComponentColumn column = createNewComponentColumn(originalColumn.getLayoutComponent(),
+                                              columnToReplace.getColumnWidth(),
+                                              false);
+
+            column.setId(columnToReplace.getId());
 
             int index = columns.indexOf(columnToReplace);
 
-            if (column instanceof ComponentColumn) {
-                // if it is a ComponentColumn we must regenerate to reload all the styling
-                ComponentColumn originalColumn = (ComponentColumn) column;
-                column = createNewComponentColumn(originalColumn.getLayoutComponent(),
-                                                  columnToReplace.getColumnWidth(),
-                                                  false);
-            }
-
             // promoting the remaining child on the actual row
-            columns.set(index,
-                        column);
+            columns.set(index, column);
 
             // destroy current column & update view
             columnToReplace.preDestroy();
