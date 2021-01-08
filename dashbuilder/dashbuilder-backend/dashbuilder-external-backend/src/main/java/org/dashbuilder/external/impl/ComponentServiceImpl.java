c764f8d12af6aa2a274aa938ea7f14e66fdbe1d5
William Siqueira <william.fatecsjc@gmail.com>
2021-01-08 11:17:36 -0300

    Enabling Component Service test

--- dashbuilder/dashbuilder-backend/dashbuilder-external-backend/src/main/java/org/dashbuilder/external/impl/ComponentServiceImpl.java
+++ dashbuilder/dashbuilder-backend/dashbuilder-external-backend/src/main/java/org/dashbuilder/external/impl/ComponentServiceImpl.java
@@ -65,7 +65,6 @@ public class ComponentServiceImpl implements ComponentService {
         
         allComponents.addAll(provided);
         allComponents.addAll(external);
-
         return allComponents;
     }
 
