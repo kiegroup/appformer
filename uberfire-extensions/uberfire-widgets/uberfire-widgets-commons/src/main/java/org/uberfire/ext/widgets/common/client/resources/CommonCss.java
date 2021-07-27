a72f90478ec14430c1359589c7624a32342278a6
Bai Xiaofeng <bxf12315@gmail.com>
2021-07-27 22:16:19 +0800

    JBPM-9670 Data type with long name overlaps tooltip area when inspecting process variables of a Process instance (#1120)

--- uberfire-extensions/uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/resources/CommonCss.java
+++ uberfire-extensions/uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/resources/CommonCss.java
@@ -104,4 +104,7 @@ public interface CommonCss
 
     @ClassName("columnPickerButton")
     String columnPickerButton();
+
+    @ClassName("popoverTextBreakWord")
+    String popoverTextBreakWord();
 }
