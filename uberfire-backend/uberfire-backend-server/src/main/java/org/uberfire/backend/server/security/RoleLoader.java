d3446a3c320c99fad48dd6b6334d6c71e6603059
Abhishek Kumar <akumar074@gmail.com>
2025-01-24 10:02:43 +0530

    DBACLD-164330: [CVE] [High] CVE-2021-33813 and more jdom-1.1.3.redhat-3.jar

--- uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/security/RoleLoader.java
+++ uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/security/RoleLoader.java
@@ -25,9 +25,9 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 
 import org.apache.commons.lang3.StringUtils;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.uberfire.backend.server.WebAppListener;
